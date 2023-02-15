f4bb58f3739b36584b42cafd197e9c2595f361b1
IhateToThinkAboutUserNames <125142223+IhateToThinkAboutUserNames@users.noreply.github.com>
2023-02-15 11:40:49 +0000

    Update pricechecker.py

--- pricechecker.py
+++ pricechecker.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 sekret = st.secrets["test"]
-
+data = pd.read_html('sekret')
 st.header(f"Separate code with 1 SPACE only !")
 with st.form('entry_form', clear_on_submit=True):
     st.write("DB username:", st.secrets["test"])

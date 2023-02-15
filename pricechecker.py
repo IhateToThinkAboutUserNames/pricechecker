40834b05ea86e43d4726adcdbf087aa9a63edb2f
IhateToThinkAboutUserNames <125142223+IhateToThinkAboutUserNames@users.noreply.github.com>
2023-02-15 08:05:36 +0000

    Update pricechecker.py

--- pricechecker.py
+++ pricechecker.py
@@ -1,36 +1,37 @@
-from io import StringIO
-from deta import Deta
+# from io import StringIO
+# from deta import Deta
 import pandas as pd
 import streamlit as st
 
-key = st.secrets(DETA_KEY)
+# key = st.secrets(DETA_KEY)
 
-deta = Deta('key')
-drive = deta.Drive("App")
-hello = drive.get('Book3.csv')
-content = hello.read()
-hello.close()
-s=str(content,'utf-8')
+# deta = Deta('key')
+# drive = deta.Drive("App")
+# hello = drive.get('Book3.csv')
+# content = hello.read()
+# hello.close()
+# s=str(content,'utf-8')
 
-data = StringIO(s) 
+# data = StringIO(s) 
 
-df=pd.read_csv(data)
+df=pd.read_csv(st.secrets("DATA"))
+return df.head()
 
-df['Product code'] = df['Product code'].str.lower()
-codes = input('Put your character here')
+# df['Product code'] = df['Product code'].str.lower()
+# codes = input('Put your character here')
 
-listt = codes.split()
-prices = []
+# listt = codes.split()
+# prices = []
 
-for i in listt:
-    for text in df["Product code"]:
-        if i in text:
-            prices.append(text)
-        else:
-            continue
+# for i in listt:
+#     for text in df["Product code"]:
+#         if i in text:
+#             prices.append(text)
+#         else:
+#             continue
 
-prlist = pd.DataFrame(df.loc[df['Product code'].isin(prices)])
-sup = prlist['Supplier'] = "Medartis"
-prlist.drop(labels=['Supplier'], axis=1,inplace=True)
-prlist.insert(2, 'Supplier', sup)
-prlist
+# prlist = pd.DataFrame(df.loc[df['Product code'].isin(prices)])
+# sup = prlist['Supplier'] = "Medartis"
+# prlist.drop(labels=['Supplier'], axis=1,inplace=True)
+# prlist.insert(2, 'Supplier', sup)
+# prlist

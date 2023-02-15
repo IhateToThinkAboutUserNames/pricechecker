f5e846eccaf440c2730f11e5f484e01941962de9
IhateToThinkAboutUserNames <125142223+IhateToThinkAboutUserNames@users.noreply.github.com>
2023-02-15 11:19:44 +0000

    Update pricechecker.py

--- pricechecker.py
+++ pricechecker.py
@@ -7,3 +7,6 @@ st.header(f"Separate code with 1 SPACE only !")
 with st.form('entry_form', clear_on_submit=True):
     ol1, col2 = st.columns(2)
     col1.selectbox("Select Supplier", sekret, key='suppliers')
+    submitted = st.form_submit_button('Get Your Codes')
+    if submitted:
+        st.success("Got your codes !")

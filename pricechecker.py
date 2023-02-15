dc5e35ae0fd177bd3be6d238ba3e8d781ace9db6
IhateToThinkAboutUserNames <125142223+IhateToThinkAboutUserNames@users.noreply.github.com>
2023-02-15 06:37:20 +0000

    Update pricechecker.py

--- pricechecker.py
+++ pricechecker.py
@@ -10,7 +10,6 @@ Please fill up the code you need and i will generete the file for you.
 
 
 codes = st.text_input('Enter your codes here')
-df = pd.read_html(r'https://nhs-my.sharepoint.com/:x:/r/personal/artur_lechowicz_nhs_net/Documents/Book3.csv?d=wa076147f5455434a8168de382ee892e4&csf=1&web=1&e=icJ14l')
 df['Product code'] = df['Product code'].str.lower()
 
 listt = codes.split()

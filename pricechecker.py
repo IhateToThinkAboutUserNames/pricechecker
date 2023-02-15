d5ed1acdd501eb39aac6b045d84988ef31b07a59
IhateToThinkAboutUserNames <125142223+IhateToThinkAboutUserNames@users.noreply.github.com>
2023-02-15 11:12:34 +0000

    Update pricechecker.py

--- pricechecker.py
+++ pricechecker.py
@@ -1,37 +1,49 @@
-# from io import StringIO
-# from deta import Deta
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-# key = st.secrets(DETA_KEY)
+page_title = "Medartis pricer checker"
+suppliers = ['Medartis']
+secret = st.secrets("DATA")
+df = pd.read_csv('secret')
+df['Product code'] = df['Product code'].str.lower()
 
-# deta = Deta('key')
-# drive = deta.Drive("App")
-# hello = drive.get('Book3.csv')
-# content = hello.read()
-# hello.close()
-# s=str(content,'utf-8')
+st.set_page_config(page_title=page_title, layout="centered")
+st.title(page_title)
 
-# data = StringIO(s) 
+st.header(f"Separate code with 1 SPACE only !")
+with st.form('entry_form', clear_on_submit=True):
+    col1, col2 = st.columns(2)
+    col1.selectbox("Select Supplier", suppliers, key='suppliers')
+    codes = st.text_input('', placeholder="Enter codes here ...")
 
-df=pd.read_csv(st.secrets("DATA"))
-return df.head()
+    "---"
+    submitted = st.form_submit_button('Get Your Codes')
+    if submitted:
+        st.success("Got your codes !")
+        listt = codes.split()
+        prices = []
 
-# df['Product code'] = df['Product code'].str.lower()
-# codes = input('Put your character here')
+        for i in listt:
+            for text in df["Product code"]:
+                if i in text:
+                    prices.append(text)
 
-# listt = codes.split()
-# prices = []
+        prlist = pd.DataFrame(df.loc[df['Product code'].isin(prices)])
+        sup = prlist['Supplier'] = "Medartis"
+        prlist.drop(labels=['Supplier'], axis=1,inplace=True)
+        prlist.insert(2, 'Supplier', sup)
+        st.dataframe(prlist)
+    else:
+        print("nope")
+    
 
-# for i in listt:
-#     for text in df["Product code"]:
-#         if i in text:
-#             prices.append(text)
-#         else:
-#             continue
 
-# prlist = pd.DataFrame(df.loc[df['Product code'].isin(prices)])
-# sup = prlist['Supplier'] = "Medartis"
-# prlist.drop(labels=['Supplier'], axis=1,inplace=True)
-# prlist.insert(2, 'Supplier', sup)
-# prlist
+
+
+
+
+
+# st.header("Price Report")
+# with st.form("Raport"):
+#     if submitted:
+#         col1.metric("Your List",)
